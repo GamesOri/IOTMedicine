14f5d1c313d537414e77895792c12c9b16a63eaf
games <gamesnologo@gmail.com>
2018-11-06 14:58:40 +0700

    Near OK

--- app/src/main/java/kampanat/nakarin/thitipong/iotmedicine/MainActivity.java
+++ app/src/main/java/kampanat/nakarin/thitipong/iotmedicine/MainActivity.java
@@ -1,11 +1,13 @@
 package kampanat.nakarin.thitipong.iotmedicine;
-
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     int greenInt = 0xFF00FF00;
     int redInt = 0xFFFF0000;
     private boolean statusABoolean = true;
+    private boolean tempABoolean = false, humidABoolean = false;
 
 
     @Override
@@ -40,8 +43,53 @@ public class MainActivity extends AppCompatActivity {
 //        RealTime Service
         realTimeService();
 
+//        Stop Alert
+        stopAlert();
 
     }   // Main Method
+
+    private void stopAlert() {
+        Button button = findViewById(R.id.btnStopAlert);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (statusABoolean) {
+                    Toast.makeText(MainActivity.this, "Click When Alert",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Stop Buzzer Alert",
+                            Toast.LENGTH_SHORT).show();
+                    stopBuzzer();
+
+                }
+
+
+
+            }   // onClick
+        });
+
+
+    }
+
+    private void stopBuzzer() {
+
+        String[] strings = new String[]{"MyTemp", "MyHumid"};
+        String[] strings1 = new String[]{"alertTemp", "alertHumid"};
+
+        for (int i = 0; i < strings.length; i++) {
+
+            FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+            DatabaseReference databaseReference = firebaseDatabase.getReference().child(strings[i]);
+
+            Map<String, Object> objectMap = new HashMap<>();
+            objectMap.put(strings1[i], 0);
+            databaseReference.updateChildren(objectMap);
+
+        }   // for
+
+
+    }
 
     private void realTimeService() {
 
@@ -66,26 +114,35 @@ public class MainActivity extends AppCompatActivity {
 
                         //                Check Temp
                         if (findInt(currentTempString) > alertTempAnInt) {
+                            tempABoolean = true;
                             setupAlert(true);
                         }
 
                         //                Check Humid
                         if (findInt(currentHumidString) > alertHumidAnInt) {
+                            humidABoolean = true;
                             setupAlert(false);
                         }
 
                     }else {
 
                         //                Check Temp
-                        if (findInt(currentTempString) <= alertTempAnInt) {
+                        if ((findInt(currentTempString) <= alertTempAnInt) && tempABoolean) {
                             tempTextView.setTextColor(greenInt);
+                            statusABoolean = true;
+                            tempABoolean = false;
+                            stopBuzzer();
                         }
 
                         //                Check Humid
-                        if (findInt(currentHumidString) <= alertHumidAnInt) {
+                        if ((findInt(currentHumidString) <= alertHumidAnInt) && humidABoolean) {
                             humidTextView.setTextColor(greenInt);
+                            statusABoolean = true;
+                            humidABoolean = false;
+                            stopBuzzer();
                         }
                     }
+
 
 
 
